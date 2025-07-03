89d8a37466b2bcd161dacae89fb6b8f085596bb4
figoaljufrie <figoaldjoeffri@gmail.com>
2025-07-03 09:07:59 +0700

    Learning Project

---
+++ Module-1/Programming-fundamental-day-04/Exercise.ts
@@ -0,0 +1,14 @@
+/*Exercise Example:
+- Create a function that receiving array as input.
+- This function can find maximum value in array without using built-in method in Java/typscript.
+- Paramaters: Array
+- Output: Number
+
+Example input: [10, 55, 79, 32]
+Example output: 79
+
+Pseudocode:
+- Bikin let array number
+
+*/
+
